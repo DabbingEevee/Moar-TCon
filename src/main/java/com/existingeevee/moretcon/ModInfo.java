d3cee3c3c3a8625d6186c87aef8ad8312e25f913
DabbingEevee <57885577+DabbingEevee@users.noreply.github.com>
2023-07-22 00:07:04 -0500

    updated version

--- src/main/java/com/existingeevee/moretcon/ModInfo.java
+++ src/main/java/com/existingeevee/moretcon/ModInfo.java
@@ -3,7 +3,7 @@ package com.existingeevee.moretcon;
 public class ModInfo {
     public static final String MODID = "moretcon";
     public static final String NAME = "More TCon";
-    public static final String VERSION = "1.10.0";
+    public static final String VERSION = "1.10.1";
     public static final boolean BETA = true;
     public static final String DEPENDANCY =
     		"required-after:tconstruct;" +
