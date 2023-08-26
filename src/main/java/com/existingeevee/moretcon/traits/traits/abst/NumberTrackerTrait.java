f930001280236a3b6fce30d38d4d3c5df44b1ec6
DabbingEevee <57885577+DabbingEevee@users.noreply.github.com>
2023-08-26 00:02:44 -0500

    Create NumberTrackerTrait.java

---
+++ src/main/java/com/existingeevee/moretcon/traits/traits/abst/NumberTrackerTrait.java
@@ -0,0 +1,76 @@
+package com.existingeevee.moretcon.traits.traits.abst;
+
+import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NBTTagCompound;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.entity.player.ItemTooltipEvent;
+import net.minecraftforge.fml.common.eventhandler.EventPriority;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
+import slimeknights.tconstruct.library.modifiers.ModifierNBT;
+import slimeknights.tconstruct.library.traits.AbstractTrait;
+import slimeknights.tconstruct.library.utils.TinkerUtil;
+
+public abstract class NumberTrackerTrait extends AbstractTrait {
+
+	protected boolean showNumberRemaining = true;
+	
+	public NumberTrackerTrait(String identifier, int color) {
+		super(identifier, color);
+		MinecraftForge.EVENT_BUS.register(this);
+	}
+
+	public abstract int getNumberMax(ItemStack stack);
+
+	public abstract int getNumberRemaining(ItemStack stack);
+
+	public abstract int setNumberRemaining(ItemStack stack, int amount);
+
+	public int addNumberRemaining(ItemStack stack, int amount) {
+		return setNumberRemaining(stack, getNumberRemaining(stack) + amount);
+	}
+
+	public int subtractNumberRemaining(ItemStack stack, int amount) {
+		return addNumberRemaining(stack, -amount);
+	}
+
+	@Override
+	public String getTooltip(NBTTagCompound modifierTag, boolean detailed) {
+		if (!showNumberRemaining) 
+			return super.getTooltip(modifierTag, detailed);
+		
+		StringBuilder sb = new StringBuilder();
+
+		ModifierNBT data = ModifierNBT.readTag(modifierTag);
+
+		sb.append(getLocalizedName());
+		if (data.level > 1) {
+			sb.append(" ");
+			sb.append(TinkerUtil.getRomanNumeral(data.level));
+		}
+		if (!detailed)
+			sb.append(": -{-toreplace.moretcon.number." + this.getIdentifier() + "-}-");
+
+		return sb.toString();
+	}
+
+	@SideOnly(Side.CLIENT)
+	@SubscribeEvent(priority = EventPriority.LOWEST)
+	public void onItemTooltipEvent(ItemTooltipEvent event) {
+		if (!showNumberRemaining) 
+			return;
+		ItemStack tool = event.getItemStack();
+		if (!this.isToolWithTrait(tool))
+			return;
+		for (int i = 0; i < event.getToolTip().size(); i++) {
+			String str = event.getToolTip().get(i);
+			String[] splitString = str.split(": ");
+			if (splitString.length >= 2 && splitString[1].equals("-{-toreplace.moretcon.number." + this.getIdentifier() + "-}-")) {
+				splitString[1] = getNumberRemaining(tool) + "/" + getNumberMax(tool);
+				event.getToolTip().set(i, String.join(": ", splitString));
+				return;
+			}
+		}
+	}
+}
