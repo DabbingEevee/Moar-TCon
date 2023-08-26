dd18d72004a26d6e02b1615a2ee05bcd9e7d98f7
DabbingEevee <57885577+DabbingEevee@users.noreply.github.com>
2023-08-25 23:18:42 -0500

    Update IBetweenTinkerTool.java

--- src/main/java/com/existingeevee/moretcon/compat/betweenlands/IBetweenTinkerTool.java
+++ src/main/java/com/existingeevee/moretcon/compat/betweenlands/IBetweenTinkerTool.java
@@ -2,6 +2,6 @@ package com.existingeevee.moretcon.compat.betweenlands;
 
 public interface IBetweenTinkerTool {
 
-	//empty intrface to locate tools that should be able to work in bl
+	//empty interface to locate tools that should be able to work in bl
 
 }
