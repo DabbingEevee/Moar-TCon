fe66b87228e3ce856056fa8b30861d30ceb88e1c
DabbingEevee <57885577+DabbingEevee@users.noreply.github.com>
2023-12-20 00:14:15 -0600

    and now were done releasing

--- src/main/java/com/existingeevee/moretcon/ModInfo.java
+++ src/main/java/com/existingeevee/moretcon/ModInfo.java
@@ -3,8 +3,8 @@ package com.existingeevee.moretcon;
 public class ModInfo {
     public static final String MODID = "moretcon";
     public static final String NAME = "More TCon";
-    public static final String VERSION = "1.11.0";
-    public static final boolean BETA = false;
+    public static final String VERSION = "1.12.0";
+    public static final boolean BETA = true;
     public static final String DEPENDANCY =
     		"required-after:tconstruct;" +
     		"after:twilightforest;" +
