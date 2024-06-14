cc8be254b4bbd7692d83f480e14f4b41d6ea392f
DabbingEevee <57885577+DabbingEevee@users.noreply.github.com>
2024-06-14 12:24:08 -0500

    fixed crash bug

--- src/main/java/com/existingeevee/moretcon/inits/recipes/FurnaceInit.java
+++ src/main/java/com/existingeevee/moretcon/inits/recipes/FurnaceInit.java
@@ -22,44 +22,60 @@ public class FurnaceInit {
 		}
 				
 		if (CompatManager.twilightforest) {
-			if (ConfigHandler.shouldLoadDust && ConfigHandler.shouldLoadDustForCompatability) {
-				GameRegistry.addSmelting(new ItemStack(ModItems.dustKnightmetal, 1), new ItemStack(TFItems.knightmetal_ingot, 1), 0F);
-				GameRegistry.addSmelting(new ItemStack(ModItems.dustFiery, 1), new ItemStack(TFItems.fiery_ingot, 1), 0F);
-				GameRegistry.addSmelting(new ItemStack(ModItems.dustPenguinite, 1), new ItemStack(ModItems.ingotPenguinite, 1), 0F);
-				GameRegistry.addSmelting(new ItemStack(ModItems.dustIronwood, 1), new ItemStack(TFItems.ironwood_ingot, 1), 0F);
-			}
+			initTF();
 		}
 		if (CompatManager.loadMain) {
-			if (ConfigHandler.shouldLoadDust) {
-				GameRegistry.addSmelting(new ItemStack(ModItems.dustFusionite, 1), new ItemStack(ModItems.ingotFusionite, 1), 0F);
-				GameRegistry.addSmelting(new ItemStack(ModItems.dustValasium, 1), new ItemStack(ModItems.ingotValasium, 1), 0F);
-				GameRegistry.addSmelting(new ItemStack(ModItems.dustIrradium, 1), new ItemStack(ModItems.ingotIrradium, 1), 0F);
-				GameRegistry.addSmelting(new ItemStack(ModItems.dustSolarSteel, 1), new ItemStack(ModItems.ingotSolsteel, 1), 0F);
-				GameRegistry.addSmelting(new ItemStack(ModItems.dustGallium, 1), new ItemStack(ModItems.ingotGallium, 1), 0F);
-				GameRegistry.addSmelting(new ItemStack(ModItems.dustRuneSteel, 1), new ItemStack(ModItems.ingotRuneSteel, 1), 0F);
-				GameRegistry.addSmelting(new ItemStack(ModItems.dustGravitonium, 1), new ItemStack(ModItems.ingotGravitonium, 1), 0F);
-				GameRegistry.addSmelting(new ItemStack(ModItems.dustTrichromadentium, 1), new ItemStack(ModItems.ingotTrichromadentium, 1), 0F);
-				GameRegistry.addSmelting(new ItemStack(ModItems.dustAtronium, 1), new ItemStack(ModItems.ingotAtronium, 1), 0F);
-				GameRegistry.addSmelting(new ItemStack(ModItems.dustSteel, 1), new ItemStack(ModItems.ingotSteel, 1), 0F);
-				GameRegistry.addSmelting(new ItemStack(ModItems.dustHallowsite, 1), new ItemStack(ModItems.ingotHallowsite, 1), 0F);
-			}
-			GameRegistry.addSmelting(new ItemStack(ModItems.rawSteel, 1), new ItemStack(ModItems.ingotSteel, 1), 0F);
-			GameRegistry.addSmelting(new ItemStack(ModBlocks.oreGravitoniumDense, 1), new ItemStack(ModItems.ingotGravitonium, 4), 0F);
+			initMain();
 		}
 		if (CompatManager.aether_legacy) {
-			if (ConfigHandler.shouldLoadDust && ConfigHandler.shouldLoadDustForCompatability) {
-				GameRegistry.addSmelting(new ItemStack(ModItems.dustArkenium, 1), new ItemStack(ModItems.ingotArkenium, 1), 0F);
-				GameRegistry.addSmelting(new ItemStack(ModItems.dustValkyrieMetal, 1), new ItemStack(ModItems.ingotValkyrieMetal, 1), 0F);
-			}
+			initAL();
 		}
 		if (CompatManager.thebetweenlands) {
-			if (ConfigHandler.shouldLoadDust && ConfigHandler.shouldLoadDustForCompatability) {
-				GameRegistry.addSmelting(new ItemStack(ModItems.dustSwampSteel, 1), new ItemStack(ModItems.ingotSwampSteel, 1), 0F);
-				GameRegistry.addSmelting(new ItemStack(ModItems.dustOctine, 1), new ItemStack(ItemRegistry.OCTINE_INGOT, 1), 0F);
-				GameRegistry.addSmelting(new ItemStack(ModItems.dustSyrmorite, 1), EnumItemMisc.SYRMORITE_INGOT.create(1), 0F);
-				GameRegistry.addSmelting(new ItemStack(ModItems.dustRotiron, 1), new ItemStack(ModItems.ingotRotiron, 1), 0F);
-				GameRegistry.addSmelting(new ItemStack(ModItems.dustAncientAlloy, 1), new ItemStack(ModItems.ingotAncientAlloy, 1), 0F);
-			}
+			initBL();
+		}
+	}
+	
+	public static void initTF() {
+		if (ConfigHandler.shouldLoadDust && ConfigHandler.shouldLoadDustForCompatability) {
+			GameRegistry.addSmelting(new ItemStack(ModItems.dustKnightmetal, 1), new ItemStack(TFItems.knightmetal_ingot, 1), 0F);
+			GameRegistry.addSmelting(new ItemStack(ModItems.dustFiery, 1), new ItemStack(TFItems.fiery_ingot, 1), 0F);
+			GameRegistry.addSmelting(new ItemStack(ModItems.dustPenguinite, 1), new ItemStack(ModItems.ingotPenguinite, 1), 0F);
+			GameRegistry.addSmelting(new ItemStack(ModItems.dustIronwood, 1), new ItemStack(TFItems.ironwood_ingot, 1), 0F);
+		}
+	}
+	
+	public static void initMain() {
+		if (ConfigHandler.shouldLoadDust) {
+			GameRegistry.addSmelting(new ItemStack(ModItems.dustFusionite, 1), new ItemStack(ModItems.ingotFusionite, 1), 0F);
+			GameRegistry.addSmelting(new ItemStack(ModItems.dustValasium, 1), new ItemStack(ModItems.ingotValasium, 1), 0F);
+			GameRegistry.addSmelting(new ItemStack(ModItems.dustIrradium, 1), new ItemStack(ModItems.ingotIrradium, 1), 0F);
+			GameRegistry.addSmelting(new ItemStack(ModItems.dustSolarSteel, 1), new ItemStack(ModItems.ingotSolsteel, 1), 0F);
+			GameRegistry.addSmelting(new ItemStack(ModItems.dustGallium, 1), new ItemStack(ModItems.ingotGallium, 1), 0F);
+			GameRegistry.addSmelting(new ItemStack(ModItems.dustRuneSteel, 1), new ItemStack(ModItems.ingotRuneSteel, 1), 0F);
+			GameRegistry.addSmelting(new ItemStack(ModItems.dustGravitonium, 1), new ItemStack(ModItems.ingotGravitonium, 1), 0F);
+			GameRegistry.addSmelting(new ItemStack(ModItems.dustTrichromadentium, 1), new ItemStack(ModItems.ingotTrichromadentium, 1), 0F);
+			GameRegistry.addSmelting(new ItemStack(ModItems.dustAtronium, 1), new ItemStack(ModItems.ingotAtronium, 1), 0F);
+			GameRegistry.addSmelting(new ItemStack(ModItems.dustSteel, 1), new ItemStack(ModItems.ingotSteel, 1), 0F);
+			GameRegistry.addSmelting(new ItemStack(ModItems.dustHallowsite, 1), new ItemStack(ModItems.ingotHallowsite, 1), 0F);
+		}
+		GameRegistry.addSmelting(new ItemStack(ModItems.rawSteel, 1), new ItemStack(ModItems.ingotSteel, 1), 0F);
+		GameRegistry.addSmelting(new ItemStack(ModBlocks.oreGravitoniumDense, 1), new ItemStack(ModItems.ingotGravitonium, 4), 0F);
+	}
+	
+	public static void initAL() {
+		if (ConfigHandler.shouldLoadDust && ConfigHandler.shouldLoadDustForCompatability) {
+			GameRegistry.addSmelting(new ItemStack(ModItems.dustArkenium, 1), new ItemStack(ModItems.ingotArkenium, 1), 0F);
+			GameRegistry.addSmelting(new ItemStack(ModItems.dustValkyrieMetal, 1), new ItemStack(ModItems.ingotValkyrieMetal, 1), 0F);
+		}
+	}
+	
+	public static void initBL() {
+		if (ConfigHandler.shouldLoadDust && ConfigHandler.shouldLoadDustForCompatability) {
+			GameRegistry.addSmelting(new ItemStack(ModItems.dustSwampSteel, 1), new ItemStack(ModItems.ingotSwampSteel, 1), 0F);
+			GameRegistry.addSmelting(new ItemStack(ModItems.dustOctine, 1), new ItemStack(ItemRegistry.OCTINE_INGOT, 1), 0F);
+			GameRegistry.addSmelting(new ItemStack(ModItems.dustSyrmorite, 1), EnumItemMisc.SYRMORITE_INGOT.create(1), 0F);
+			GameRegistry.addSmelting(new ItemStack(ModItems.dustRotiron, 1), new ItemStack(ModItems.ingotRotiron, 1), 0F);
+			GameRegistry.addSmelting(new ItemStack(ModItems.dustAncientAlloy, 1), new ItemStack(ModItems.ingotAncientAlloy, 1), 0F);
 		}
 	}
 }
