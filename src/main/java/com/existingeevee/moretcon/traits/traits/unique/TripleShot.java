2474b5256ae8a96df64feb2748848bbdce3c7199
DabbingEevee <57885577+DabbingEevee@users.noreply.github.com>
2024-07-08 23:40:13 -0500

    improved tripleshot code

--- src/main/java/com/existingeevee/moretcon/traits/traits/unique/TripleShot.java
+++ src/main/java/com/existingeevee/moretcon/traits/traits/unique/TripleShot.java
@@ -13,41 +13,34 @@ import slimeknights.tconstruct.library.tools.ranged.ProjectileCore;
 import slimeknights.tconstruct.library.traits.AbstractProjectileTrait;
 public class TripleShot extends AbstractProjectileTrait {
 
+	//TODO quanterion math
+	
 	public TripleShot() {
 		super(MiscUtils.createNonConflictiveName("tripleshot"), 0xFFFFFF);
         MinecraftForge.EVENT_BUS.register(this);
 	}
 
-	//@ObjectHolder("tconstruct:shuriken")
-	//public static final Item shuriken = null;
+	public static final ThreadLocal<Boolean> IS_ALREADY_PROCING = ThreadLocal.withInitial(() -> false);
 
 	@Override
 	public void onLaunch(EntityProjectileBase projectileBase, World world, EntityLivingBase shooter) {
-
-		StackTraceElement[] stacktrace = Thread.currentThread().getStackTrace();
-
-		int found = 0;
-
-		for (StackTraceElement e : stacktrace) {
-			if (e.getClassName().equals(this.getClass().getName())) {
-				if (found++ >= 1) {
-					return;
-				}
-			}
-		}
+		if (IS_ALREADY_PROCING.get())
+			return;
 
 		if(projectileBase != null && shooter != null) {
 			TinkerProjectileHandler ticProjectile = projectileBase.tinkerProjectile;
-			if(!world.isRemote) {
+			if(!world.isRemote && ticProjectile.getItemStack().getItem() instanceof ProjectileCore) {
+				IS_ALREADY_PROCING.set(true);
 				EntityProjectileBase projOne = ((ProjectileCore) ticProjectile.getItemStack().getItem()).getProjectile(ticProjectile.getItemStack().copy(), ticProjectile.getLaunchingStack().copy(), projectileBase.getEntityWorld(), shooter instanceof EntityPlayer ? (EntityPlayer) shooter : null, 2.1f, 0f, 1f, false);
 				world.spawnEntity(projOne);
 				projOne.pickupStatus = PickupStatus.CREATIVE_ONLY;
 				projOne.shoot(shooter, shooter.rotationPitch, shooter.rotationYaw - 11.25f, 0, 2f, 0);
-
+								
 			    EntityProjectileBase projTwo = ((ProjectileCore) ticProjectile.getItemStack().getItem()).getProjectile(ticProjectile.getItemStack().copy(), ticProjectile.getLaunchingStack().copy(), projectileBase.getEntityWorld(), shooter instanceof EntityPlayer ? (EntityPlayer) shooter : null, 2.1f, 0f, 1f, false);
 			    world.spawnEntity(projTwo);
 				projTwo.pickupStatus = PickupStatus.CREATIVE_ONLY;
 				projTwo.shoot(shooter, shooter.rotationPitch, shooter.rotationYaw + 11.25f, 0, 2f, 0);
+				IS_ALREADY_PROCING.set(false);
 			}
 			if (shooter instanceof EntityPlayer) {
 				((EntityPlayer) shooter).getCooldownTracker().setCooldown(ticProjectile.getLaunchingStack().getItem(), 10);
