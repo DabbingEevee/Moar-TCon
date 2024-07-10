d9a66a67fdbbfebd8666cc8415d93b4db67775f8
DabbingEevee <57885577+DabbingEevee@users.noreply.github.com>
2024-07-10 15:02:25 -0500

    Update Offense.java

--- src/main/java/com/existingeevee/moretcon/traits/traits/unique/Offense.java
+++ src/main/java/com/existingeevee/moretcon/traits/traits/unique/Offense.java
@@ -22,6 +22,7 @@ public class Offense extends AbstractTrait {
 		if (tool.getItem() instanceof ToolCore) {
 			int ticksSinceLastSwing = ObfuscationReflectionHelper.getPrivateValue(EntityLivingBase.class, player, "field_184617_aD");
 			ObfuscationReflectionHelper.setPrivateValue(EntityLivingBase.class, player, Integer.MAX_VALUE, "field_184617_aD");
+			event.getSource().getImmediateSource().hurtResistantTime = 0;
 			ToolHelper.attackEntity(tool, (ToolCore) tool.getItem(), player, event.getSource().getImmediateSource(), null, false);
 			ObfuscationReflectionHelper.setPrivateValue(EntityLivingBase.class, player, ticksSinceLastSwing, "field_184617_aD");
 		}
