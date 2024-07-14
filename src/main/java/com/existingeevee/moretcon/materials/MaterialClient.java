b30a55bfe138e6c0d79590cece8cb3e3fe113380
DabbingEevee <57885577+DabbingEevee@users.noreply.github.com>
2024-07-13 20:50:39 -0500

    valasium

--- src/main/java/com/existingeevee/moretcon/materials/MaterialClient.java
+++ src/main/java/com/existingeevee/moretcon/materials/MaterialClient.java
@@ -54,6 +54,7 @@ public class MaterialClient {
 		ModMaterials.materialOctine.setRenderInfo(new GlintTexture.RenderInfo(0xff8906, 0xd3550c, 0xf7f7f7, 0xffc81f, 0xf8a100, 0xff8906));
 		ModMaterials.materialAncientAlloy.setRenderInfo(new LightShadingTextureColoredTexture.RenderInfo(new ResourceLocation(ModInfo.MODID + ":other/material" + ModMaterials.materialAncientAlloy.getIdentifier().replaceFirst(ModInfo.MODID + ".", ""))));
 		ModMaterials.materialAtronium.setRenderInfo(new GlintTexture.RenderInfo(0x4e4031, 0x2f261c, 0xf0ceac, 0xf6be86, 0xd99857, 0xd99857));
+		ModMaterials.materialValasium.setRenderInfo(new GlintTexture.RenderInfo(0x646681, 0x646681, 0xb3c3cb, 0x959cb3, 0x959cb3, 0x959cb3));
 	}
 
 	private static void setCustomRender(Material mat) {
