d2c5d78ad011c32bcb55c2b8735fdfba9eba7440
DabbingEevee <57885577+DabbingEevee@users.noreply.github.com>
2024-07-16 18:31:47 -0500

    make tripleshot use quanterions

--- src/main/java/com/existingeevee/moretcon/traits/traits/unique/TripleShot.java
+++ src/main/java/com/existingeevee/moretcon/traits/traits/unique/TripleShot.java
@@ -1,23 +1,24 @@
 package com.existingeevee.moretcon.traits.traits.unique;
 
+import com.existingeevee.math.Quaternion;
 import com.existingeevee.moretcon.other.utils.MiscUtils;
 
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.entity.projectile.EntityArrow.PickupStatus;
+import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.World;
 import net.minecraftforge.common.MinecraftForge;
 import slimeknights.tconstruct.library.capability.projectile.TinkerProjectileHandler;
 import slimeknights.tconstruct.library.entity.EntityProjectileBase;
 import slimeknights.tconstruct.library.tools.ranged.ProjectileCore;
 import slimeknights.tconstruct.library.traits.AbstractProjectileTrait;
+
 public class TripleShot extends AbstractProjectileTrait {
 
-	//TODO quanterion math
-	
 	public TripleShot() {
 		super(MiscUtils.createNonConflictiveName("tripleshot"), 0xFFFFFF);
-        MinecraftForge.EVENT_BUS.register(this);
+		MinecraftForge.EVENT_BUS.register(this);
 	}
 
 	public static final ThreadLocal<Boolean> IS_ALREADY_PROCING = ThreadLocal.withInitial(() -> false);
@@ -27,19 +28,12 @@ public class TripleShot extends AbstractProjectileTrait {
 		if (IS_ALREADY_PROCING.get())
 			return;
 
-		if(projectileBase != null && shooter != null) {
+		if (projectileBase != null && shooter != null) {
 			TinkerProjectileHandler ticProjectile = projectileBase.tinkerProjectile;
-			if(!world.isRemote && ticProjectile.getItemStack().getItem() instanceof ProjectileCore) {
+			if (!world.isRemote && ticProjectile.getItemStack().getItem() instanceof ProjectileCore) {
 				IS_ALREADY_PROCING.set(true);
-				EntityProjectileBase projOne = ((ProjectileCore) ticProjectile.getItemStack().getItem()).getProjectile(ticProjectile.getItemStack().copy(), ticProjectile.getLaunchingStack().copy(), projectileBase.getEntityWorld(), shooter instanceof EntityPlayer ? (EntityPlayer) shooter : null, 2.1f, 0f, 1f, false);
-				world.spawnEntity(projOne);
-				projOne.pickupStatus = PickupStatus.CREATIVE_ONLY;
-				projOne.shoot(shooter, shooter.rotationPitch, shooter.rotationYaw - 11.25f, 0, 2f, 0);
-								
-			    EntityProjectileBase projTwo = ((ProjectileCore) ticProjectile.getItemStack().getItem()).getProjectile(ticProjectile.getItemStack().copy(), ticProjectile.getLaunchingStack().copy(), projectileBase.getEntityWorld(), shooter instanceof EntityPlayer ? (EntityPlayer) shooter : null, 2.1f, 0f, 1f, false);
-			    world.spawnEntity(projTwo);
-				projTwo.pickupStatus = PickupStatus.CREATIVE_ONLY;
-				projTwo.shoot(shooter, shooter.rotationPitch, shooter.rotationYaw + 11.25f, 0, 2f, 0);
+				shootProjectile(ticProjectile, shooter, -11.25f);
+				shootProjectile(ticProjectile, shooter, 11.25f);
 				IS_ALREADY_PROCING.set(false);
 			}
 			if (shooter instanceof EntityPlayer) {
@@ -48,14 +42,15 @@ public class TripleShot extends AbstractProjectileTrait {
 		}
 	}
 
+	public static void shootProjectile(TinkerProjectileHandler ticProjectile, EntityLivingBase shooter, float angle) {
+		EntityProjectileBase proj = ((ProjectileCore) ticProjectile.getItemStack().getItem()).getProjectile(ticProjectile.getItemStack().copy(), ticProjectile.getLaunchingStack().copy(), shooter.world, shooter instanceof EntityPlayer ? (EntityPlayer) shooter : null, 2.1f, 0f, 1f, false);
+		shooter.world.spawnEntity(proj);
+		proj.pickupStatus = PickupStatus.CREATIVE_ONLY;
 
-	/*private static Entity copyEntity(Entity e) {
-		NBTTagCompound data = e.writeToNBT(new NBTTagCompound());
-		Entity entityCopy = EntityRegistry.getEntry(e.getClass()).newInstance(e.world);
-		entityCopy.readFromNBT(data);
-		entityCopy.setUniqueId(MathHelper.getRandomUUID());
-		return entityCopy;
-	}*/
+		Vec3d vec31 = shooter.getVectorForRotation(shooter.rotationPitch - 90, shooter.rotationYaw);
+		Quaternion quaternion = new Quaternion(vec31, angle, true);
+		Vec3d vec3 = shooter.getLookVec();
+		vec3 = quaternion.transformVector(vec3);
+		proj.shoot(vec3.x, vec3.y, vec3.z, 2f, 0);
+	}
 }
-
-//end bricks
