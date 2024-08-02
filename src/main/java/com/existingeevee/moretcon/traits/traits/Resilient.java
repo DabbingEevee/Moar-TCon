c4b6206763ab942785c7cc8c7d1bca8f1b0929b9
DabbingEevee <57885577+DabbingEevee@users.noreply.github.com>
2024-08-02 15:39:07 -0500

    buff to resil

--- src/main/java/com/existingeevee/moretcon/traits/traits/Resilient.java
+++ src/main/java/com/existingeevee/moretcon/traits/traits/Resilient.java
@@ -8,18 +8,26 @@ import net.minecraft.item.ItemStack;
 import net.minecraft.potion.PotionEffect;
 import slimeknights.tconstruct.library.traits.AbstractTrait;
 
-
 public class Resilient extends AbstractTrait {
 
 	public Resilient() {
 		super(MiscUtils.createNonConflictiveName("resilient"), 0x8e3acf);
 	}
+
 	@Override
 	public int onToolDamage(ItemStack tool, int damage, int newDamage, EntityLivingBase entity) {
-	    if (random.nextBoolean() && random.nextBoolean() && !entity.isPotionActive(MobEffects.MINING_FATIGUE)) {
-	    	entity.addPotionEffect(new PotionEffect(MobEffects.MINING_FATIGUE, damage * 40, 0, false, true));
-	    	return (newDamage - 1 >= 0) ? newDamage - 1 : 0;
-	    }
-	    return newDamage;
+		if (random.nextBoolean() && random.nextBoolean() && !entity.isPotionActive(MobEffects.MINING_FATIGUE)) {
+			entity.addPotionEffect(new PotionEffect(MobEffects.MINING_FATIGUE, damage * 40, 0, false, true));
+			return (newDamage - 1 >= 0) ? newDamage - 1 : 0;
+		}
+		return newDamage;
+	}
+
+	@Override
+	public float damage(ItemStack tool, EntityLivingBase player, EntityLivingBase target, float damage, float newDamage, boolean isCritical) {
+		if (player.isPotionActive(MobEffects.MINING_FATIGUE)) {
+			return newDamage + damage * 0.5f;
+		}
+		return newDamage;
 	}
 }
