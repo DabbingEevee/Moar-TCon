782e061e9b8667f8ebae9ec40beb877305644f99
DabbingEevee <57885577+DabbingEevee@users.noreply.github.com>
2026-08-24 16:25:20 -0500

    nothingburger

--- src/test/java/com/existingeevee/moretcon/mathtest/MathTest.java
+++ src/test/java/com/existingeevee/moretcon/mathtest/MathTest.java
@@ -1,14 +1,22 @@
 package com.existingeevee.moretcon.mathtest;
 
-import com.existingeevee.math.linear_algebra.LinearAlgebraUtils;
-import com.existingeevee.math.linear_algebra.VecNd;
-
-import scala.actors.threadpool.Arrays;
+import net.minecraft.init.Biomes;
+import net.minecraft.init.Bootstrap;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+import net.minecraft.world.biome.Biome;
 
 public class MathTest {
 	public static void main(String... args) {
-		System.out.println(Arrays.toString(LinearAlgebraUtils.gramSchmidt(new VecNd(0, 0, 1, 1), new VecNd(0, 1, 1, 0), new VecNd(1, 1, 0, 0))));
-		System.out.println(Arrays.toString(new VecNd[] { new VecNd(0, 0, 1, 1).scale(1. / Math.sqrt(2)), new VecNd(0, 1, 1. / 2, -1. / 2).scale(Math.sqrt(2d / 3)), new VecNd(3, 1, -1, 1).scale(1d / (2 * Math.sqrt(3))) }));
+		// System.out.println(Arrays.toString(LinearAlgebraUtils.gramSchmidt(new VecNd(0, 0, 1, 1), new VecNd(0, 1, 1, 0), new VecNd(1, 1, 0, 0))));
+//		System.out.println(Arrays.toString(new VecNd[] { new VecNd(0, 0, 1, 1).scale(1. / Math.sqrt(2)), new VecNd(0, 1, 1. / 2, -1. / 2).scale(Math.sqrt(2d / 3)), new VecNd(3, 1, -1, 1).scale(1d / (2 * Math.sqrt(3))) }));
+		
+		Bootstrap.register();
+		
+		System.out.println(calcAridiculousness(Biomes.EXTREME_HILLS));
+	}
 
+	protected static float calcAridiculousness(Biome biome) {
+		return (float) (Math.pow(1.25, 3d * (0.5f + biome.getDefaultTemperature() - biome.getRainfall())) - 1.25d);
 	}
 }
