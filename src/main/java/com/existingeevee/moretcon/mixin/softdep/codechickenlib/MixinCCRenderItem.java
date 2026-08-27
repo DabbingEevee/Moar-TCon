ba90ea46b6aa223e0edeb10652aac1f27138a69a
DabbingEevee <57885577+DabbingEevee@users.noreply.github.com>
2026-08-27 01:17:08 -0500

    why did i do that

--- src/main/java/com/existingeevee/moretcon/mixin/softdep/codechickenlib/MixinCCRenderItem.java
+++ src/main/java/com/existingeevee/moretcon/mixin/softdep/codechickenlib/MixinCCRenderItem.java
@@ -9,16 +9,24 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import com.existingeevee.moretcon.other.SlotRendererRegistry;
 
 import codechicken.lib.render.item.CCRenderItem;
+import net.minecraft.client.renderer.RenderItem;
 import net.minecraft.client.renderer.block.model.IBakedModel;
+import net.minecraft.client.renderer.block.model.ModelManager;
+import net.minecraft.client.renderer.color.ItemColors;
+import net.minecraft.client.renderer.texture.TextureManager;
 import net.minecraft.item.ItemStack;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 
 @Mixin(CCRenderItem.class)
-public abstract class MixinCCRenderItem {
+public abstract class MixinCCRenderItem extends RenderItem {
+
+	public MixinCCRenderItem(TextureManager p_i46552_1_, ModelManager p_i46552_2_, ItemColors p_i46552_3_) {
+		super(p_i46552_1_, p_i46552_2_, p_i46552_3_);
+	}
 
 	@SideOnly(Side.CLIENT)
-	@Inject(at = @At("HEAD"), method = { "renderItemModelIntoGUI", "func_191962_a" }, cancellable = true, remap = false)
+	@Inject(at = @At("HEAD"), method = "renderItemModelIntoGUI", cancellable = true)
 	protected void moretcon$HEAD_Inject$renderItemModelIntoGUI(ItemStack stack, int x, int y, IBakedModel bakedmodel, CallbackInfo ci) {
 		if (!this.isValidModel(bakedmodel)) { // We only have this execute IF chickenlib is handling it
 			return; // Otherwise its handled in the vanilla RenderItem
