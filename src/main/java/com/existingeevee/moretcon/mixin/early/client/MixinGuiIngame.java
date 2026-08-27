6779617fa9a150b03159fd09e2b8dd3b2c5f8e58
DabbingEevee <57885577+DabbingEevee@users.noreply.github.com>
2026-08-27 01:16:41 -0500

    use mixinextras to make the overwrite less ass

--- src/main/java/com/existingeevee/moretcon/mixin/early/client/MixinGuiIngame.java
+++ src/main/java/com/existingeevee/moretcon/mixin/early/client/MixinGuiIngame.java
@@ -1,12 +1,15 @@
 package com.existingeevee.moretcon.mixin.early.client;
 
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Overwrite;
 import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
 
 import com.existingeevee.moretcon.other.utils.ReequipHack;
+import com.llamalad7.mixinextras.expression.Definition;
+import com.llamalad7.mixinextras.expression.Expression;
+import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
+import com.llamalad7.mixinextras.sugar.Local;
 
-import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.GuiIngame;
 import net.minecraft.item.ItemStack;
 
@@ -14,54 +17,13 @@ import net.minecraft.item.ItemStack;
 public class MixinGuiIngame {
 
 	@Shadow
-	Minecraft mc;
-	@Shadow
-	int remainingHighlightTicks;
-	@Shadow
 	ItemStack highlightingItemStack;
-	@Shadow
-    protected int updateCounter;
-	@Shadow
-    protected int overlayMessageTime;
-	@Shadow
-    protected int titlesTimer;
-	@Shadow
-    protected String displayedTitle;
-	@Shadow
-    protected String displayedSubTitle;
-
-	@Overwrite
-	public void updateTick() {
-		if (this.overlayMessageTime > 0) {
-			--this.overlayMessageTime;
-		}
-
-		if (this.titlesTimer > 0) {
-			--this.titlesTimer;
-
-			if (this.titlesTimer <= 0) {
-				this.displayedTitle = "";
-				this.displayedSubTitle = "";
-			}
-		}
-
-		++this.updateCounter;
-
-		if (this.mc.player != null) {
-			ItemStack itemstack = this.mc.player.inventory.getCurrentItem();
-
-			if (itemstack.isEmpty()) {
-				this.remainingHighlightTicks = 0;
-			} else if (ReequipHack.similarStackForActionBar(highlightingItemStack, itemstack)) {
-				if (this.remainingHighlightTicks > 0) {
-					--this.remainingHighlightTicks;
-				}
-			} else {
-				this.remainingHighlightTicks = 40;
-			}
-
-			this.highlightingItemStack = itemstack;
-		}
+	
+	@Definition(id = "areItemStackTagsEqual", method = "Lnet/minecraft/item/ItemStack;areItemStackTagsEqual(Lnet/minecraft/item/ItemStack;Lnet/minecraft/item/ItemStack;)Z")
+	@Expression("areItemStackTagsEqual(?, ?)")
+	@ModifyExpressionValue(method = "updateTick", at = @At("MIXINEXTRAS:EXPRESSION"))
+	private boolean moretcon$EXPRESSION_areItemStackTagsEqual_ModifyExpressionValue$updateTick(boolean original, @Local ItemStack itemstack) {
+		return original || ReequipHack.similarStackForActionBar(highlightingItemStack, itemstack);
 	}
 
 }
